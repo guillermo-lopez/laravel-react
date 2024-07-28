50f00b4df93d316e102ba0f5087be6320f5ce233
Guillermo Lopez <glopez686@gmail.com>
2024-07-27 23:40:47 -0400

    update Guest layout

--- stubs/react-router/resources/js/Layouts/GuestLayout.jsx
+++ stubs/react-router/resources/js/Layouts/GuestLayout.jsx
@@ -1,11 +1,11 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import {Link} from "react-router-dom";
 
 export default function Guest({ children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
-                <Link href="/">
+                <Link to="/">
                     <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
                 </Link>
             </div>
